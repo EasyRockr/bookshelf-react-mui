4fc5ee155f1d1011e7a8f3e6e079752b92023730
RobinMrnD <150664500+RobinMrnD@users.noreply.github.com>
2025-09-21 10:51:40 +0800

    Patch: Added Burger Menu Icon and fixed some layouts

--- src/App.jsx
+++ src/App.jsx
@@ -2,4 +2,10 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppShell from './components/layout/AppShell.jsx'
 import Router from './app/routes/Router.jsx'
-export default function App(){return(<BrowserRouter><AppShell><Router/></AppShell></BrowserRouter>)}
+export default function App(){return(
+    <BrowserRouter>
+        <AppShell>
+            <Router/>
+        </AppShell>
+    </BrowserRouter>
+)}
